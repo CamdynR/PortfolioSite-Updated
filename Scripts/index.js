93d1855e0c44980895d8a926b055fe4a4c5377fa
CamdynR <rasque.camdyn@gmail.com>
2020-01-17 12:00:36 -0800

    First commit

---
+++ Scripts/index.js
@@ -0,0 +1,13 @@
+// Main Script Page
+$('map').imageMapResize();
+
+new fullpage('#fullpage', {
+  licenseKey: 'A712E8FC-09014D74-A019D174-DB018286',
+  controlArrows: true,
+  css3: true,
+  dragAndMove: true,
+  navigation: true,
+  lazyLoading: true,
+  loopBottom: true,
+  sectionsColor: ['#f7f04d', 'white', '#5B7F64', '#595959'],
+});
